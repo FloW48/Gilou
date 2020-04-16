3a0a5b12a70b70ba1f25380f4aa9ad25d5323aa8
FloW48 <41864763+FloW48@users.noreply.github.com>
2020-04-16 12:53:21 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyCOd5EAiPgTdRY8xqmJzBNahI9WdhiEwoA');
-
-youtube.searchVideos('Centuries', 4)
-    .then(results => {
-        console.log(`The video's title is ${results[0].title}`);
-    })
-    .catch(console.log);
