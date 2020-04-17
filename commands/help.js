86d4af1fd6e66db6a2068eb55e8f7db3e9d78ac6
FloW48 <41864763+FloW48@users.noreply.github.com>
2020-04-18 00:46:24 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -7,8 +7,7 @@ module.exports = class Help extends Command{
     }
 
     static action(message){
-        console.log('get called kiddo')
-        const embed = new Discord.RichEmbed();
+        const embed = new Discord.MessageEmbed();
         embed.setTitle('Besoin d\'aide ? Gilou est là pour ça !');
         embed.setColor('#ff33cc');
         embed.setDescription('!gplay + Mots Clés ou Lien ```Effectue une recherche sur YouTube en cas de mots clés, et séléctionne le premier résultat.\nSi vous entrez un lien de vidéo, le son sera joué, si vous entrez un lien de playlist, toutes les musiques seront ajoutées à la file d\'attente``` \n\n !gskip : ```Passe à la musique suivante``` \n\n !gnp ```Donne le nom de la musique qui se joue actuellement```\n\n !gstop : ```Déconnecte Gilou et enlève toutes les belles chansons qu\'il voulait jouer de la file d\'attente```')
