3038e30fc27775e419be544bd2ad418934d17f2b
FloW48 <41864763+FloW48@users.noreply.github.com>
2020-06-04 00:12:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ const bot = new Discord.Client();
 const Play = require('./commands/play');
 const Help = require('./commands/help')
 
+var messageActivyl = 
+
 bot.on('ready', function(){
     bot.user.setActivity("!ghelp pour les commandes").catch(console.error);
 })
 
 
 bot.on('message', function (message){
+    console.log(message);
     let commandUsed =   Play.parse(message) ||
                         Help.parse(message);
 })
 
 bot.login('NTk2MzM4MDA5MDc1NjEzNzAz.Xpgg4A.ojXFJNxS0oZGywzHkoHtpypLbPo')
     .catch(console.error);
+
+
+var millisecondsToWait = 10000;
+var d;
+var oldHour = -1;
+var hour = -1;
+alvityl()
+
+function alvityl(){
+    if( (hour >= 0 && hour <= 8) || (hour >= 22 && hour <=23)){
+        if(oldHour != hour){
+            //Play.playActivyl(bot)
+            oldHour = hour
+        }
+    }
+    setTimeout(function() {
+        d = new Date();
+        hour = d.getHours()
+        console.log(hour)
+        alvityl()
+    }, millisecondsToWait);
+}
