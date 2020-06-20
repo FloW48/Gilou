525c848b7c50efa1085e4ee9739104a428d1537d
FloW48 <41864763+FloW48@users.noreply.github.com>
2020-06-20 12:13:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const Play = require('./commands/play');
 const Help = require('./commands/help')
 const Fnatic = require('./commands/fnatic')
 
-var messageActivyl = 
+
+var millisecondsToWait = 100;
+var d;
+var oldHour = -1;
+var hour = -1;
+var dateToInge = new Date(2024, 6, 1);
+
 
 bot.on('ready', function(){
     bot.user.setActivity("!ghelp pour les commandes").catch(console.error);
+    bot.channels.fetch("210094361428492289")  
+        .then(channel => 
+            setTimeout(function() {
+                d = new Date();
+                hour = d.getHours()
+                alvityl()
+                day_to_ingenieur(channel)
+            }, millisecondsToWait))
+        .catch(console.error);
 })
-
 
 bot.on('message', function (message){
     let commandUsed =   Play.parse(message) ||
@@ -20,13 +34,6 @@ bot.on('message', function (message){
 bot.login('NTk2MzM4MDA5MDc1NjEzNzAz.Xpgg4A.ojXFJNxS0oZGywzHkoHtpypLbPo')
     .catch(console.error);
 
-
-var millisecondsToWait = 10000;
-var d;
-var oldHour = -1;
-var hour = -1;
-alvityl()
-
 function alvityl(){
     if(hour >= 20 || hour <= 6){
         if(oldHour != hour){
@@ -34,9 +41,13 @@ function alvityl(){
             oldHour = hour
         }
     }
-    setTimeout(function() {
-        d = new Date();
-        hour = d.getHours()
-        alvityl()
-    }, millisecondsToWait);
+}
+
+function day_to_ingenieur(channel){
+    if(hour == 9){
+        const diffTime = Math.abs(dateToInge - d);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        console.log(diffDays);
+        channel.send("<@256054054260572161> <@266220917791653889> <@202170302401085440> Il vous reste **__"+ diffDays+ "__** jours avant de devenir ingé !!")
+    }
 }
