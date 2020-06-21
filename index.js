46acb543bcc95930daa8e9b1df016d4963564860
FloW48 <41864763+FloW48@users.noreply.github.com>
2020-06-22 00:08:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -14,15 +14,7 @@ var dateToInge = new Date(2024, 6, 1);
 
 bot.on('ready', function(){
     bot.user.setActivity("!ghelp pour les commandes").catch(console.error);
-    bot.channels.fetch("210094361428492289")  
-        .then(channel => 
-            setTimeout(function() {
-                d = new Date();
-                hour = d.getHours()
-                alvityl()
-                day_to_ingenieur(channel)
-            }, millisecondsToWait))
-        .catch(console.error);
+    timer()
 })
 
 bot.on('message', function (message){
@@ -34,26 +26,38 @@ bot.on('message', function (message){
 bot.login('NTk2MzM4MDA5MDc1NjEzNzAz.Xpgg4A.ojXFJNxS0oZGywzHkoHtpypLbPo')
     .catch(console.error);
 
+
+function timer(){
+    setTimeout(function() {
+        console.log("ptdrr")
+        d = new Date();
+        hour = d.getHours()
+        alvityl()
+        day_to_ingenieur()
+        oldHour = hour
+        timer()
+        return
+    }, millisecondsToWait)
+}
+
 function alvityl(){
     if(hour >= 20 || hour <= 6){
         if(oldHour != hour){
             Play.playActivyl(bot)
-            oldHour = hour
         }
     }
 }
 
-function day_to_ingenieur(channel){
-    if(hour == 11){
+function day_to_ingenieur(){
+    if(hour == 6){
         if(oldHour != hour){
+            console.log("yo")
             const diffTime = Math.abs(dateToInge - d);
             const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
             console.log(diffDays);
-            channel.send("<@256054054260572161> <@266220917791653889> <@202170302401085440> Il vous reste **__"+ diffDays+ "__** jours avant de devenir ingé !!")
-            oldHour = hour
+            channel = bot.channels.fetch('210094361428492289').then(channel =>{
+                channel.send("<@256054054260572161> <@202170302401085440> <@266220917791653889> Il vous reste **__"+ diffDays+ "__** jours avant de devenir ingé !!")
+            })
         }
-    }
-    else{
-        oldHour = hour
     }
 }
